694846982bd688d8713222e4607e8fc1cc950e5a
dr02899 <dr02899@georgiasouthern.edu>
2016-11-06 12:26:36 -0500

    Example commit for git lab 2

---
+++ Palindrome/src/TestForPalindrome.java
@@ -0,0 +1,39 @@
+ 
+/*
+    A palindrome is a word that reads the same backward or forward.
+
+	Write a function that checks if a given word is a palindrome. Character case should be ignored.
+
+	For example, isPalindrome("Deleveled") should return true as character case should be ignored, 
+	resulting in "deleveled", which is a palindrome since it reads the same backward and forward.
+
+ */
+
+
+public class TestForPalindrome {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(TestForPalindrome.isPalindrome("Deleveled"));
+
+
+	}
+	
+	public static boolean isPalindrome(String word)
+	{
+		String fixedWord = word.toLowerCase();
+		int length = fixedWord.length();
+		
+		for(int i = 0; i <= fixedWord.length()-1; i++)
+		{
+			if(fixedWord.charAt(i) != fixedWord.charAt((length-1) - i))
+			{
+				return false;
+			}
+			
+		}
+		
+		return true;
+	}
+
+}
